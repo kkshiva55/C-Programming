7dca7460d8d76d1287269da50b3815d7c52e3899
Shiva K <111070201+kkshiva55@users.noreply.github.com>
2022-08-29 19:22:14 +0530

    Update a2_positive_fibonacci.c

--- Assignments/1_Basic Refresher/a2_positive_fibonacci.c
+++ Assignments/1_Basic Refresher/a2_positive_fibonacci.c
@@ -19,46 +19,31 @@ Sample output:  --> Enter a number: 6
 
 int main ()
 {
-    int num,sum = 0,first = 0, second = 1;   // Initialisation of required variables
-
+    int num, sum = 0, first = 0, second = 1;   // Initialisation of required variables
     printf("Enter a number: ");    // Getting input from the user
-
     scanf("%d", &num);    // Storing the value at the address of variable "num"
-
     printf("\n");       // Printing output on a new line
-
 
         if(num < 0)       // True if user has given negative value
         {
-
-        printf("Invalid input");     // Printing Error message
-
+            printf("Invalid input");     // Printing Error message
         }
-
 
         else
         {
+            do     // Fibonacci series upto the given range
+            {
+                if (sum != 0)
+                {
+                    printf(", ");
+                }
 
-            printf("%d", first);      // Printing the first element
+                  printf("%d", sum);    // Printing the Fibonacci series which is separated by commas
+                  first = second;      // Storing previous two values
+                  second = sum;
+                  sum = first + second;    // Adding previous two values
 
-               while(sum <= num)       // Fibonacci series upto the given range
-               {
-
-                   first = second;      // Storing previous two values
-                   second = sum;
-
-                   sum = first + second;    // Adding previous two values
-
-                        if(sum <= num)     // Checking if number is within the given range
-                        {
-
-                           printf(", %d", sum);    // Printing the Fibonacci series which is separated by commas
-
-                        }
-
-
-               }
-
+             } while (sum <= num);
          }
 
     return 0;
