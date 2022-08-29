7e66b68d1376b07de4a9f375c1753912ccc74194
Shiva K <111070201+kkshiva55@users.noreply.github.com>
2022-08-29 19:31:10 +0530

    Update a3_negative_fibonacci.c

--- Assignments/1_Basic Refresher/a3_negative_fibonacci.c
+++ Assignments/1_Basic Refresher/a3_negative_fibonacci.c
@@ -21,71 +21,30 @@ Sample output:  --> Enter a number: -20
 int main ()
 {
     int num, sum = 0, first = 0, second = 1;   // Initialization of required variables
-
     printf("Enter a number: ");    // Getting input from the user
-
     scanf("%d", &num);    // Storing the value at the address of variable "num"
-
     printf("\n");       // Printing output on a new line
-
 
         if(num > 0)       // True if user has given positive value
         {
-
-        printf("Invalid input");     // Printing Error message
-
+            printf("Invalid input");     // Printing Error message
         }
-
 
         else
         {
+            while(sum >= num && -sum >= num)       // Fibonacci series upto the given range
+            {
+                if (sum != 0)
+                {
+                    printf(", ");
+                }
 
-            printf("%d", first);      // Printing the first element
-
-               while(sum > num)       // Fibonacci series upto the given range
-               {
-
-                   first = second;      // Storing previous two values
-                   second = sum;
-
-                   sum = first - second;    // Subtracting previous two values
-
-
-
-                   /* Negative Fibonacci series consists of alternative plus and minus terms, so we have to
-                      check sum in two ways to ensure the output is within the range */
-
-
-                      if(sum < 0)      // True if sum is negative
-                      {
-
-
-                            if(-sum <= -num)       // Checking if sum is within the given range
-                            {
-
-
-                                printf(", %d", sum);    // Printing the sum value which is separated by commas
-
-                            }
-
-                      }
-
-                      else      // Sum is positive
-                       {
-
-
-                            if(sum <= -num)         // Checking if sum is within the given range
-                            {
-
-                                 printf(", %d", sum);     // Printing the sum value which is separated by commas
-
-                            }
-
-                       }
-
-               }
-
-        }
+                  printf("%d", sum);
+                  first = second;      // Storing previous two values
+                  second = sum;
+                  sum = first - second;    // Subtracting previous two values
+            }
+         }
 
     return 0;
 }
